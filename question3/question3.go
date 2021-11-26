cc1c8061b268715c8ae7fa4a60f751e2f31dd061
andira14 <clown_is_stupid@yahoo.com>
2021-11-27 00:14:22 +0700

    question3: Added main function

--- question3/question3.go
+++ question3/question3.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 )
 
@@ -17,4 +18,15 @@ func findFirstStringInBracket(str string) string {
 		}
 	}
 	return ""
+}
+
+func main() {
+	fmt.Println(findFirstStringInBracket("(test string)"))
+	fmt.Println(findFirstStringInBracket("()"))
+	fmt.Println(findFirstStringInBracket("(lala"))
+	fmt.Println(findFirstStringInBracket("lala)"))
+	fmt.Println(findFirstStringInBracket("(test string 2)"))
+	fmt.Println(findFirstStringInBracket("(asdf)(test)"))
+	fmt.Println(findFirstStringInBracket("((a(a(a)"))
+	fmt.Println(findFirstStringInBracket("((aaa))"))
 }
