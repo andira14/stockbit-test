46b085518a8f1f46a4b1f1ad39a7312e78ba36aa
andira14 <clown_is_stupid@yahoo.com>
2021-11-28 18:22:05 +0700

    question2: Added get detail repo

--- question2/src/movies/repository.go
+++ question2/src/movies/repository.go
@@ -8,8 +8,8 @@ import (
 	"questions/question2/src/mapper"
 )
 
-func getMovies(page string, searchWord string) (searchData, error) {
-	url := fmt.Sprintf("%s&s=%s&page=%s", imdbUrlSetup(), searchWord, page)
+func getRepo(page int, searchWord string) (searchData, error) {
+	url := fmt.Sprintf("%s&s=%s&page=%d", imdbUrlSetup(), searchWord, page)
 	resp, err := http.Get(url)
 	if err != nil {
 		return searchData{}, err
@@ -25,6 +25,28 @@ func getMovies(page string, searchWord string) (searchData, error) {
 	err = json.Unmarshal(bodyBytes, &result)
 	if err != nil {
 		return searchData{}, err
+	}
+
+	return result, nil
+}
+
+func getDetailRepo(imdbId string) (detail, error) {
+	url := fmt.Sprintf("%s&i=%s", imdbUrlSetup(), imdbId)
+	resp, err := http.Get(url)
+	if err != nil {
+		return detail{}, err
+	}
+
+	bodyBytes, jsonError := mapper.JsonToBytes(resp.Body)
+	if jsonError != nil {
+		return detail{}, jsonError
+	}
+
+	var result detail
+
+	err = json.Unmarshal(bodyBytes, &result)
+	if err != nil {
+		return detail{}, err
 	}
 
 	return result, nil
