19303d8805b120debcd576731a189d9187b8e4e7
andira14 <clown_is_stupid@yahoo.com>
2021-11-28 18:22:40 +0700

    question2: Added structs for detail movies

--- question2/src/movies/interfaces.go
+++ question2/src/movies/interfaces.go
@@ -1,6 +1,6 @@
 package movies
 
-type movieData struct {
+type data struct {
 	Title     string `json:"Title"`
 	Year      string `json:"Year"`
 	Id        string `json:"imdbID"`
@@ -9,8 +9,28 @@ type movieData struct {
 }
 
 type searchData struct {
-	Search      []movieData `json:"Search"`
-	ResultCount string      `json:"totalResults"`
-	Response    string      `json:"Response"`
-	Error       string      `json:"Error"`
+	Search      []data `json:"Search"`
+	ResultCount string `json:"totalResults"`
+	Response    string `json:"Response"`
+	Error       string `json:"Error,omitempty"`
+}
+
+type rating struct {
+	Source string `json:"Source"`
+	Value  string `json:"Value"`
+}
+type detail struct {
+	Title     string   `json:"Title"`
+	Year      string   `json:"Year"`
+	Id        string   `json:"imdbID"`
+	MovieType string   `json:"Type"`
+	Poster    string   `json:"Poster"`
+	Released  string   `json:"Released"`
+	Genre     string   `json:"Genre"`
+	Director  string   `json:"Director"`
+	Writer    string   `json:"Writer"`
+	Actors    string   `json:"Actors"`
+	Ratings   []rating `json:"Ratings"`
+	Response  string   `json:"Response"`
+	Error     string   `json:"Error,omitempty"`
 }
